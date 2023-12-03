405642d089ccd19e8202f05d7d4d4faf8e6e7d37
xblask05 <xblask05@vutbr.cz>
2023-12-03 21:28:35 +0100

    feat: Added database migration from version 1 to version 2

--- app/src/main/java/com/example/zenglow/data/GroupDatabase.kt
+++ app/src/main/java/com/example/zenglow/data/GroupDatabase.kt
@@ -1,7 +1,11 @@
 package com.example.zenglow.data
 
+import androidx.room.AutoMigration
 import androidx.room.Database
+import androidx.room.RenameColumn
+import androidx.room.RenameTable
 import androidx.room.RoomDatabase
+import androidx.room.migration.AutoMigrationSpec
 import com.example.zenglow.data.entities.Device
 import com.example.zenglow.data.entities.Group
 
@@ -11,7 +15,14 @@ import com.example.zenglow.data.entities.Group
         Device::class,
     ],
     version = 2,
+    autoMigrations = [
+        AutoMigration (from = 1, to = 2, spec = GroupDatabase.MyAutoMigration::class)
+    ]
+
 )
 abstract class GroupDatabase: RoomDatabase() {
+    @RenameColumn(tableName = "group", fromColumnName = "id", toColumnName = "groupId")
+    class MyAutoMigration : AutoMigrationSpec
+
     abstract val dao: GroupDao
 }
